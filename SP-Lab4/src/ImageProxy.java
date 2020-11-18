80266f8408f43561d1d36729d28b0be7e0b48a75
PadureanDaniel <72490288+MeXicaNu1@users.noreply.github.com>
2020-11-18 11:46:44 +0200

    Add files via upload

---
+++ SP-Lab4/src/ImageProxy.java
@@ -0,0 +1,23 @@
+public class ImageProxy implements Element {
+    private String url;
+    private Dimension dimension;
+    private Image realImage;
+
+    public ImageProxy(String url, Dimension dimension){
+        this.url = url;
+        this.dimension = dimension;
+    }
+
+    private Image loadImage(){
+        if(realImage == null){
+            realImage = new Image(url);
+        }
+        return realImage;
+    }
+
+    @Override
+    public void print() {
+        loadImage().print();
+        dimension.printDimension();
+    }
+}
